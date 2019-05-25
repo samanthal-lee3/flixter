63cd677bf993bb35eaff5c5815cc71971a774a99
Samantha Lee <samanthal.lee3@gmail.com>
2019-05-25 21:02:37 +0000

    building create action for the instructor course controller

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,7 +1,18 @@
 class Instructor::CoursesController < ApplicationController
     before_action :authenticate_user!
-    
-    def new
 
+    def new
+      @course = Course.new
+    end
+
+    def create
+      @course = current_user.courses.create(course_params)
+      redirect-to instructor_course_path(@course)
+    end
+
+    private
+
+    def course_params
+      params.require(:course).permit(:title, :description, :cost)
     end
 end
