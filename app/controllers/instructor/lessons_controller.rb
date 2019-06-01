6c0d66f153e02fa5ecc3f13a869dd6d3a50ed422
Samantha Lee <samanthal.lee3@gmail.com>
2019-06-01 19:22:34 +0000

    working add lesson button using the lesson controller create action

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -5,5 +5,14 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def create
+    @section = Section.find(params[:section_id])
+    @lesson = @section.lessons.create(lesson_params)
+    redirect_to instructor_course_path(@section.course)
+  end
+
+  private
+  
+  def lesson_params
+    params.require(:lesson).permit(:title, :subtitle)
   end
 end
--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -18,3 +18,4 @@ class Instructor::SectionsController < ApplicationController
 
 end
 
+
