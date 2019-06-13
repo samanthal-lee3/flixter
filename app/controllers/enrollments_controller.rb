cd86696fbf809f241c21bdc7b96f0e3c4bfd9c60
Samantha Lee <samanthal.lee3@gmail.com>
2019-06-13 20:47:08 +0000

    Built enrolled_in? method in user model to test t/f if a user is enrolled

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,6 +3,7 @@ class EnrollmentsController < ApplicationController
 
 def create
   current_user.enrollments.create(course: current_course)
+  redirect_to course_path(current_course)
 end
 
 private
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def enrolled_in?(course)
+    enrolled_courses[]
+    enrollments.each do |enrollment|
+      enrolled_courses << enrollment.course
+    end
+
+  return enrolled_courses.include?(course)
+  end 
+
 end
