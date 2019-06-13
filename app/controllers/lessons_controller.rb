e765e47341c9d2a48786be2895754acc9078a6c9
Samantha Lee <samanthal.lee3@gmail.com>
2019-06-13 21:39:05 +0000

    Setting limitations for user view in the lessons controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,23 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.user != enrolled_in?
+      redirect_to course_path, alert: 'You must enroll to view lesson details'
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
 end
+
