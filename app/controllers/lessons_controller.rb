34f8c0a55b9372a8d9d0507ea3391ac0182b2525
Samantha Lee <samanthal.lee3@gmail.com>
2019-06-14 19:22:17 +0000

    Attempting to build current_user helper method to prevent error

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -9,14 +9,19 @@ class LessonsController < ApplicationController
   private
 
   def require_authorized_for_current_lesson
-    if current_lesson.user != current_lesson.enrolled_in?
+    if current_user != enrolled_in?(current_lesson)
       redirect_to course_path, alert: 'You must enroll to view lesson details'
     end
   end
 
   helper_method :current_lesson
+  helper_method :current_user
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def current_user
+    @current_user ||= User.find(params[:id])
   end
 
 end
